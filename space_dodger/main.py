2195355bde745cf0e7a7a3675101d05a7883fcf7
Allan Ramogo <amramogo@gmail.com>
2024-11-04 17:46:25 +0300

    End game when player is hit

--- space_dodger/main.py
+++ space_dodger/main.py
@@ -23,18 +23,23 @@ start_time = time.time()
 elapsed_time = 0
 end_time = time.time() - start_time
 
-FONT = pygame.font.SysFont("roboto", 30)
+FONT = pygame.font.SysFont(
+    "roboto",
+    30,
+    bold=True,
+    italic=True,
+)
 
-projectile_add_interval = 2000  # 2 seconds in milliseconds
-projectile_count = 0  # Counter for when to add a projectile
+
+# projectile_count = 0  # Counter for when to add a projectile
 
 projectiles = []
 
 PROJECTILE_WIDTH = 10
 PROJECTILE_HEIGHT = 10
-PROJECTILE_VELOCITY = 10
+PROJECTILE_VELOCITY = 3
 
-hit = False
+# hit = False
 
 
 def draw(player, elapsed_time, projectiles):
@@ -46,12 +51,19 @@ def draw(player, elapsed_time, projectiles):
     WINDOW.blit(elapsed_time_text, (10, 10))
 
     pygame.draw.rect(WINDOW, "red", player)
+
+    # Draw projectiles after player so that they appear 'in front' of the player
+    for projectile in projectiles:
+        pygame.draw.rect(WINDOW, "yellow", projectile)
+
     pygame.display.update()  # Update the display
 
 
 def main():
     run = True
-
+    projectile_add_interval = 2000  # 2 seconds in milliseconds
+    projectile_count = 0
+    hit = False
     player = pygame.Rect(650, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
     player.center = (
         WIDTH // 2,
@@ -60,11 +72,11 @@ def main():
 
     while run:  # Game loop
         # This is keeping count of how the number of milliseconds in 1 second (1 tick)
-        projectiles_count = clock.tick(60)  # Sets the FPS to 60.
+        projectile_count += clock.tick(60)  # Sets the FPS to 60.
         elapsed_time = time.time() - start_time
 
-        if projectiles_count > projectile_add_interval:
-            for _ in range(random.randint(1, 3)):
+        if projectile_count > projectile_add_interval:
+            for _ in range(random.randint(0, 5)):
                 projectile_x = random.randint(
                     0, WIDTH - PROJECTILE_WIDTH
                 )  # Random x position for projectile
@@ -77,7 +89,10 @@ def main():
                 projectiles.append(projectile)
 
             projectile_add_interval = max(200, projectile_add_interval - 50)
-            projectiles_count = 0
+            print(f"Number of projectiles: {len(projectiles)}")
+            print(f"Projectiles count: {projectile_count}")
+
+            projectile_count = 0
 
         # Event handling MUST be inside the loop
         for event in pygame.event.get():
@@ -108,6 +123,22 @@ def main():
                 hit = True
                 break
 
+        if hit:
+            lose_text = FONT.render("You Lose TEKO!", True, "white")
+            WINDOW.blit(
+                lose_text,
+                (
+                    WIDTH / 2 - lose_text.get_width() / 2,
+                    HEIGHT / 2 - lose_text.get_height() / 2,
+                ),
+            )
+            pygame.display.update()
+            pygame.time.delay(4000)  # 4 seconds in milliseconds
+
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                run = True
+            else:
+                break
         draw(player, elapsed_time, projectiles)
     pygame.quit()
 
